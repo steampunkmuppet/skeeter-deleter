89f5dcbe265a25af7c3318cbac12d674d9891652
steampunkmuppet <jennifer.zandstra@gmail.com>
2025-12-23 01:54:58 -0500

    Update post deletion condition in skeeter_deleter.py
    Refactor conditional logic for post deletion criteria.

--- skeeter_deleter.py
+++ skeeter_deleter.py
@@ -136,8 +136,8 @@ class PostQualifier(models.AppBskyFeedDefs.PostView):
         Returns:
             bool: True if the post should be deleted, False otherwise.
         """
-        if #(post.is_viral(viral_threshold) or 
-                    post.is_stale(stale_threshold, now) and \
+        #if (post.is_viral(viral_threshold) or 
+        if post.is_stale(stale_threshold, now) and \
             not post.is_protected_domain(domains_to_protect) and \
             not post.is_self_liked(self_likes):
             return True
